833b68d1ea1bb5273f09d53ff4aba0b28f63de16
WoodsonTD <taylordwoodson@gmail.com>
2023-05-10 13:30:04 -0500

    add

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,1 +1,8 @@
-const add = (addNeighborhood, newNeighborhood) => addNeighborhood + newNeighborhood;
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
