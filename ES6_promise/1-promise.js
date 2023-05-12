b4421d651a90a78823ad0dc2058c724782894cd6
WoodsonTD <taylordwoodson@gmail.com>
2023-05-12 12:35:44 -0500

    add

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error('the fake API is not working currently '));
+      reject(new Error('the fake API is not working currently'));
     }
   });
 }
