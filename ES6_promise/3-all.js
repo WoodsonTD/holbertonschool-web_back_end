fa50bfdd1ef8c6998d44f323fecf84266d040e21
WoodsonTD <taylordwoodson@gmail.com>
2023-05-12 13:30:55 -0500

    add

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,16 +2,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((response) => {
-    console.log(response.body);
-  }).catch((error) => {
-    console.error(error);
-  });
-
-  createUser().then((response) => {
-    console.log(response.firstName);
-    console.log(response.lastName);
-  }).catch((error) => {
-    console.error(('Signup system offline'));
-  });
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body);
+      console.log(userResponse.firstName);
+      console.log(userResponse.lastName);
+    })
+    .catch((error) => {
+      console.error('Signup system offline');
+    });
 }
