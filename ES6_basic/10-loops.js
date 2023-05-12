e96d3800b2845f11fe379336927f9eb36ec5f3ee
WoodsonTD <taylordwoodson@gmail.com>
2023-05-12 11:40:51 -0500

    add

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value;
   }
-
   return array;
 }
