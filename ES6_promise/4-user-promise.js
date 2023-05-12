182749561df0c523e5885d5723352f87ae040688
WoodsonTD <taylordwoodson@gmail.com>
2023-05-12 13:57:03 -0500

    add

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-unused-vars */
-function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
