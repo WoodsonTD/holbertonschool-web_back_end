75b15538b0a2a24965cfb7fea384e5045bf94348
WoodsonTD <taylordwoodson@gmail.com>
2023-05-16 14:59:08 -0500

    add

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return sizes.map((size) => new ClassRoom(size));
+}
+
+const rooms = initializeRooms();
+console.log(rooms);
+console.log(rooms[0]._maxStuendtsSize);
+console.log(rooms[1]._maxStudentsSize);
+console.log(rooms[2]._maxStudentsSize);
