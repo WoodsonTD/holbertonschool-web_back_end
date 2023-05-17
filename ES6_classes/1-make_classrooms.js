a7a462288c790ca0b6f36b7f7c3bfff76e49b2a0
WoodsonTD <taylordwoodson@gmail.com>
2023-05-16 20:43:42 -0500

    remove console.logs

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,14 +1,7 @@
+/* eslint-disable no-unused-vars */
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const sizes = [19, 20, 34];
   return sizes.map((size) => new ClassRoom(size));
 }
-
-const rooms = initializeRooms();
-
-console.log(rooms);
-console.log(rooms.length);
-console.log(rooms[0]._maxStudentsSize);
-console.log(rooms[1]._maxStudentsSize);
-console.log(rooms[2]._maxStudentsSize);
