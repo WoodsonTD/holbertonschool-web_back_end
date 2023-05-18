fcaa0030231fbbedef1870883f90a0b8d403c1be
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 21:05:44 -0500

    add

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,28 +1,11 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-/* eslint-disable no-unused-vars */
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
-  }
-
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    return new this.constructor();
   }
 }
