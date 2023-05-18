3f0c43262e85b443e9cd954537e5de092b013ba8
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 20:00:43 -0500

    add

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
