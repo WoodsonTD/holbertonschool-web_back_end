643595628e59e193801021f5b28e62bf4dbbc8af
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 19:38:48 -0500

    add

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,16 @@
+/* eslint-disable-next-line no-unused-vars */
 class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
 
+  /* eslint-disable-next-line no-unused-vars */
   get sqft() {
     return this._sqft;
   }
 
-  /* eslint-disable class-methods-use-this */
+  /* eslint-disable-next-line class-methods-use-this */
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-
-export default Building;
