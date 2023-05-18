2605db611f19014db39ba832daad2c5a5c8801e8
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 19:58:53 -0500

    add

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
