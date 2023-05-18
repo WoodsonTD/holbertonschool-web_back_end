63acdb4810e0fffb8f04471545b3f65c0865b97c
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 20:43:54 -0500

    add

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-unused-vars */
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+
+  return arr.map((obj) => obj.id);
 }
+
+export default getListStudentIds;
