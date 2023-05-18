6a06df65087f3b2a572b4edbe932e9fa1e9d3c09
WoodsonTD <taylordwoodson@gmail.com>
2023-05-17 20:51:42 -0500

    add

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -10,6 +10,12 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    } this._floors = newFloors;
+  }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
   }
