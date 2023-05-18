0cc94d0bbe75c694936d1b2fcab6a03329a2814e
WoodsonTD <taylordwoodson@gmail.com>
2023-05-18 09:56:46 -0500

    add

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(array) {
+    if (!Array.isArray(array)) { // if array is not an array
+      return []; // return empty array
+    }
+  
+    return array.map((item) => item.id); // return array of ids
+  }
+  
+  export default getListStudentIds; // export function
