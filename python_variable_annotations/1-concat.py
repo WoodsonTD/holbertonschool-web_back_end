d58fabb43365d57dc3a6dd5ea77ea2045199c88d
WoodsonTD <taylordwoodson@gmail.com>
2023-09-05 13:28:24 -0500

    add

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""concatenates two strings and returns the result as a string"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """args: str1 (str): the first string. str2 (str): the second string.
+    returns: str the concatenation of 'str1' and 'str2' as a string."""
+    return str1 + str2
