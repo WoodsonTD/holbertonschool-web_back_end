5021c74c4039f3f4561da15675c14c3487840009
WoodsonTD <taylordwoodson@gmail.com>
2023-09-05 13:38:47 -0500

    add

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""returns the floor of a float as an integer"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """args: n (float): the input float.
+    returns int: the floor of the input float as an integer."""
+    return math.floor(n)
