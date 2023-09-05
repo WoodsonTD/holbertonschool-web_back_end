ac92ffc815887223914162be7712f64d12c155c7
WoodsonTD <taylordwoodson@gmail.com>
2023-09-05 13:44:47 -0500

    add

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""converts a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """args: n (float): the input float.
+    returns: str: the string representation of the float."""
+    return str(n)
