ce4b151370c76887b5b0dd28b47760e2a4fa0897
WoodsonTD <taylordwoodson@gmail.com>
2023-09-06 21:14:23 -0500

    changed value

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """List[int]: a list of 10 random numbers"""
     return [i async for i in async_generator()]
