717e0fbbb742bd19642972889e765a6134262449
WoodsonTD <taylordwoodson@gmail.com>
2023-09-21 15:11:24 -0500

    add

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+"""This code defines a hash_password function that securely hashes
+a user's password using the bcrypt package. Here's a brief
+explanation of what the code does"""
 
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """generate a salt for hashing
+    has the password using the generated salt"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hash_password
