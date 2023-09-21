cc290973f9120b99c54144c0e555ef278c02242c
WoodsonTD <taylordwoodson@gmail.com>
2023-09-21 14:42:28 -0500

    str

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,9 @@
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     return re.sub(fr'({"|".join(fields)})=[^\\{separator}]*',
                 fr'\1={redaction}', message)
