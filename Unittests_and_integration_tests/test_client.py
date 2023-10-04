89ecae058964ccfdde08005fa67ce2ed5e339130
WoodsonTD <taylordwoodson@gmail.com>
2023-10-04 10:00:09 -0500

    add

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -27,7 +27,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
 
 @patch('client.GithubOrgClient._public_repos_url',
-        new_callable=PropertyMock)
+       new_callable=PropertyMock)
 def test_public_repos_url(self, mock_public_repos_url):
     """Test that the result of _public_repos_url is the expected one"""
     expected = 'www.yes.com'
@@ -47,22 +47,24 @@ def test_public_repos(self, mock_get_json):
             GithubOrgClient,
             '_public_repos_url',
             new_callable=PropertyMock
-        ) as mock_public_repos_url:
-            mock_public_repos_url.return_value = "www.yes.com"
-            test_class = GithubOrgClient("test")
-            result = test_class.public_repos()
-            self.assertEqual(result, ["repo1", "repo2"])
-            mock_public_repos_url.assert_called_once()
-            mock_get_json.assert_called_once()
+    ) as mock_public_repos_url:
+        mock_public_repos_url.return_value = "www.yes.com"
+        test_class = GithubOrgClient("test")
+        result = test_class.public_repos()
+        self.assertEqual(result, ["repo1", "repo2"])
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
+
 
 @parameterized.expand([
             ({"license": {"key": "my_license"}}, "my_license", True),
             ({"license": {"key": "other_license"}}, "my_license", False),
         ])
-    def test_has_license(self, repo, license_key, expected):
-        """Test that GithubOrgClient.has_license"""
-        test_class = GithubOrgClient("test_org")
-        self.assertEqual(test_class.has_license(repo, license_key), expected)
+def test_has_license(self, repo, license_key, expected):
+    """Test that GithubOrgClient.has_license"""
+    test_class = GithubOrgClient("test_org")
+    self.assertEqual(test_class.has_license(repo, license_key), expected)
+
 
 @parameterized_class(
         ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
