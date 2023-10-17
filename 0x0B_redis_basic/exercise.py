9712646c5c334920d7e33ccfcf1fb91b538c2e43
WoodsonTD <taylordwoodson@gmail.com>
2023-10-17 11:26:58 -0500

    add

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -28,3 +28,40 @@ class Cache:
 
     def get_int(self, key: str) -> int:
         return self.get(key, fn=int)
+
+    def count_calls(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        count_key = f"{key}:count"
+        self._redis.incr(count_key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+# Decorate Cache.store with count_calls
+Cache.store = count_calls(Cache.store)
+
+def call_history(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        inputs_key = f"{key}:inputs"
+        outputs_key = f"{key}:outputs"
+        self._redis.rpush(inputs_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(result))
+        return result
+    return wrapper
+
+# Decorate Cache.store with call_history
+Cache.store = call_history(Cache.store)
+
+def replay(cache: Cache):
+    method_name = "Cache.store"
+    inputs_key = f"{method_name}:inputs"
+    outputs_key = f"{method_name}:outputs"
+    inputs = cache._redis.lrange(inputs_key, 0, -1)
+    outputs = cache._redis.lrange(outputs_key, 0, -1)
+    for i, (input_data, output_data) in enumerate(zip(inputs, outputs):
+        print(f"{method_name} was called {i + 1} times:")
+        print(f"{method_name}({input_data}) -> {output_data}")
